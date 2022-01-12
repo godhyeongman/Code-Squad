639057d5dcef0acb41018d717f7d7a9fd3675ff6
godhyeongman <aaaa4000@naver.com>
2022-01-12 09:49:20 +0900

    링크드 리스트
    삭제 말고 구현 완료
    추가 구현 해볼것 (find 기능)
    
    삭제는 해당 id 노드 모두 삭제로 구현중

--- CS01/linkedList.js
+++ CS01/linkedList.js
@@ -30,20 +30,24 @@ class LinkedList {
     const incomingNode = new VideoData(id);
     let current, previous;
     current = this.head;
+
     if (index == 0) {
       incomingNode.next = current;
-      current = incomingNode;
+      this.head = incomingNode;
       console.log(this.showAllData());
       return;
     }
 
-    let num = +index;
+    if (index > this.clip) {
+      console.log("범위를 벗어난 숫자입니다.");
+      return;
+    }
 
-    while (--num != 0) {
-      console.log(`head: ${this.head.id}`);
+    while (index-- != 0) {
       previous = current;
       current = current.next;
     }
+
     incomingNode.next = current;
     previous.next = incomingNode;
 
@@ -51,16 +55,49 @@ class LinkedList {
     console.log(this.showAllData());
   }
 
+  remove(check) {
+    let current;
+    current = this.head;
+    if (this.clip == 1 && current.id == check) {
+      this.head = null;
+      console.log("표시할 비디오 정보가 없습니다.");
+      return;
+    }
+
+    while (current.next != null) {
+      if (current.next.id == check) {
+        console.log(loop);
+        console.log(current.id);
+        current.next = current.next.next;
+      }
+      current = current.next;
+    }
+    if (current.id == check) {
+      current.next = null;
+    }
+    console.log(this.showAllData());
+  }
+
   showAllData() {
     let answer = "|---";
     let current = this.head;
     answer += `[${current.id},${current.time}]---`;
-    while (current.next != null) {
+    while (current?.next != null) {
       current = current.next;
       answer += `[${current.id},${current.time}]---`;
     }
 
     return answer;
+  }
+
+  render() {
+    let totalTime = 0;
+    let current = this.head;
+    while (current !== null) {
+      totalTime += current.time;
+      current = current.next;
+    }
+    console.log(`영상클립:${this.clip}\n총 시간:${totalTime}`);
   }
 }
 const video = new LinkedList();
@@ -70,9 +107,11 @@ function checkCommand(input) {
   command == "add"
     ? video.appendNode(id)
     : command == "insert"
-    ? video.insertAtList(id, index)
-    : command == "delete"
-    ? video.remove[id]
+    ? video.insertAtList(id, +index)
+    : command == "remove"
+    ? video.remove(id)
+    : command == "render"
+    ? video.render()
     : console.log("잘못된 입력입니다.");
 
   if (command != "quit") start();
