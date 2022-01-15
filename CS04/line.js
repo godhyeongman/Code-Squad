ae321a7a4b74755378e9faa1dcfa7bec2705a69a
godhyeongman <88314186+godhyeongman@users.noreply.github.com>
2022-01-15 18:24:57 +0900

    super 사용해서 자식 클래스 내에서 부모메서드 사용해보기
    부모 클래스 도형에서
    자식 클래스 line Triangle에
    점을 구하는 식으로 super을 사용해봄 figure은 추상화라 최대한 기능을 안넣는 방식으로 구현을 계획중

--- CS04/line.js
+++ CS04/line.js
@@ -2,21 +2,55 @@ class Figure {
   constructor(coord) {
     this.coord = coord;
     this.line = null;
+    this.area = null;
   }
-  static getLineLength() {}
+
   //도형들이 뭘가지고 있을까?(필수요소)
+
+  getPoint(A) {
+    const x = A[0];
+    const y = A[1];
+    return [x, y];
+  }
+  getArea() {
+    console.log("이 객체는 추상화 객체입니다.");
+  }
 }
 
-class Line {
+class Line extends Figure {
   constructor(coord) {
-    super(coord);
+    super(coord, line, area);
   }
-  getLineLength() {
-    const A = this.coord[0];
-    const B = this.coord[1];
-    return Math.sqrt((A[0] - B[0]) ** 2 + (A[1] - B[1]) ** 2);
+  getLineLength(A, B) {
+    const firstPoint = super.getPoint(A[0]);
+    const secondPoint = super.getPoint(B[1]);
+    const lineLength = Math.sqrt(
+      (firstPoint[0] - secondPoint[0]) ** 2 +
+        (firstPoint[1] - secondPoint[1]) ** 2
+    );
+    return lineLength;
   }
   getArea() {
     return console.log("선은 길이만 존재합니다.");
+  }
+}
+
+class Triangle extends Figure {
+  constructor(coord) {
+    super(coord, line, area);
+  }
+  getArea() {
+    const firstLine = new Line(
+      super.getPoint(this.coord[0]),
+      super.getPoint(this.coord[1])
+    );
+    const secondPoint = new Line(
+      super.getPoint(this.coord[1]),
+      super.getPoint(this.coord[2])
+    );
+    const thirdPoint = new Line(
+      super.getPoint(this.coord[2]),
+      super.getPoint(this.coord[0])
+    );
   }
 }
