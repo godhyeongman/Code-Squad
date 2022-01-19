2c4e8a7807afc89341134e30175e3d4481507e4f
godhyeongman <aaaa4000@naver.com>
2022-01-19 14:41:11 +0900

    알고리즘 공부
    완전탐색 백트래킹 필수 문제만 풀어보았다.
    
    문제는 풀었는데  완전 탐색과 백트래킹의 정확한 동작 방식과 원리를 공부해보아야겠다.

--- CS05/heap.js
+++ CS05/heap.js
@@ -1,86 +1,87 @@
-// class Heap {
+class Heap {
+  constructor(size) {
+    this.storage = [];
+    this.size;
+  }
+
+  inputData(data) {
+    this.storage.unshift(data);
+  }
+
+  deleteData(data) {
+    this.storage.shift(data);
+  }
+
+  getAddress(data) {
+    this.storage.findIndex(data);
+  }
+}
+
+// class MaxBinaryHeap {
 //   constructor() {
-//     this.storage = [];
+//     this.values = [];
 //   }
-
-//   inputData(data) {
-//     this.storage.unshift(data);
+//   insert(element) {
+//     this.values.push(element);
+//     this.bubbleUp();
 //   }
-
-//   deleteData(data) {
-//     this.storage.shift(data);
-//   }
-
-//   getAddress(data) {
-//     this.storage.findIndex(data);
+//   bubbleUp() {
+//     let idx = this.values.length - 1;
+//     const element = this.values[idx];
+//     while (idx > 0) {
+//       let parentIdx = Math.floor((idx - 1) / 2);
+//       let parent = this.values[parentIdx];
+//       if (element <= parent) break;
+//       this.values[parentIdx] = element;
+//       this.values[idx] = parent;
+//       idx = parentIdx;
+//     }
 //   }
 // }
 
-class MaxBinaryHeap {
-  constructor() {
-    this.values = [];
-  }
-  insert(element) {
-    this.values.push(element);
-    this.bubbleUp();
-  }
-  bubbleUp() {
-    let idx = this.values.length - 1;
-    const element = this.values[idx];
-    while (idx > 0) {
-      let parentIdx = Math.floor((idx - 1) / 2);
-      let parent = this.values[parentIdx];
-      if (element <= parent) break;
-      this.values[parentIdx] = element;
-      this.values[idx] = parent;
-      idx = parentIdx;
-    }
-  }
-}
+// let heap = new MaxBinaryHeap();
+// heap.insert(41);
+// heap.insert(39);
+// heap.insert(33);
+// heap.insert(18);
+// heap.insert(27);
+// heap.insert(12);
+// heap.insert(55);
+// console.log(heap);
 
-let heap = new MaxBinaryHeap();
-heap.insert(41);
-heap.insert(39);
-heap.insert(33);
-heap.insert(18);
-heap.insert(27);
-heap.insert(12);
-heap.insert(55);
-console.log(heap);
-// module.exports = Heap;
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
+// class Node {
+//   constructor(value) {
+//     this.value = value;
+//     this.next = null;
+//   }
+// }
 
-class Stack {
-  constructor() {
-    this.first = null;
-    this.last = null;
-    this.size = 0;
-  }
-  push(val) {
-    var newNode = new Node(val);
-    if (!this.first) {
-      this.first = newNode;
-      this.last = newNode;
-    } else {
-      var temp = this.first;
-      this.first = newNode;
-      this.first.next = temp;
-    }
-    return ++this.size;
-  }
-  pop() {
-    if (!this.first) return null;
-    var temp = this.first;
-    if (this.first === this.last) {
-      this.last = null;
-    }
-    this.first = this.first.next;
-    this.size--;
-    return temp.value;
-  }
-}
+// class Stack {
+//   constructor() {
+//     this.first = null;
+//     this.last = null;
+//     this.size = 0;
+//   }
+//   push(val) {
+//     var newNode = new Node(val);
+//     if (!this.first) {
+//       this.first = newNode;
+//       this.last = newNode;
+//     } else {
+//       var temp = this.first;
+//       this.first = newNode;
+//       this.first.next = temp;
+//     }
+//     return ++this.size;
+//   }
+//   pop() {
+//     if (!this.first) return null;
+//     var temp = this.first;
+//     if (this.first === this.last) {
+//       this.last = null;
+//     }
+//     this.first = this.first.next;
+//     this.size--;
+//     return temp.value;
+//   }
+// }
---
+++ algorithm/3rdWeek1st.js
@@ -0,0 +1,60 @@
+// function solution(board, moves) {
+//   let answer = 0;
+//   const basket = [];
+//   for (let i = 0; i < moves.length; i++) {
+//     for (let j = 0; j < board.length; j++) {
+//       if (board[j][moves[i] - 1] !== 0) {
+//         basket.push(board[j][moves[i] - 1]);
+//         board[j][moves[i] - 1] = 0;
+//         break;
+//       }
+//     }
+
+//     if (
+//       basket.length > 1 &&
+//       basket[basket.length - 1] === basket[basket.length - 2]
+//     ) {
+//       basket.splice(basket.length - 2, 2);
+//       answer += 2;
+//     }
+//   }
+//   return answer;
+// }
+
+// console.log(
+//   solution(
+//     [
+//       [0, 0, 0, 0, 0],
+//       [0, 0, 1, 0, 3],
+//       [0, 2, 5, 0, 1],
+//       [4, 2, 4, 4, 2],
+//       [3, 5, 1, 3, 1],
+//     ],
+//     [1, 5, 3, 5, 1, 2, 1, 4]
+//   )
+// );
+
+function solution(answers) {
+  const bingBong = [];
+  const first = [1, 2, 3, 4, 5];
+  const second = [2, 1, 2, 3, 2, 4, 2, 5];
+  const third = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+  const result = [0, 0, 0];
+  for (let i = 0; i < answers.length; i++) {
+    const num = answers[i];
+    if (first[i % first.length] === num) result[0]++;
+    if (second[i % second.length] === num) result[1]++;
+    if (third[i % third.length] === num) result[2]++;
+  }
+  const winner = Math.max(...result);
+
+  for (let j = 0; j < result.length; j++) {
+    if (result[j] === winner) {
+      bingBong.push(j + 1);
+    }
+  }
+
+  return bingBong;
+}
+
+console.log(solution([1, 3, 2, 4, 2]));
