b57a0356f06ea76d1b2172e571cf97e28b0f2b37
godhyeongman <aaaa4000@naver.com>
2022-01-24 13:51:09 +0900

    no message

--- CS06/classifierAlpha.js
+++ CS06/classifierAlpha.js
@@ -2,15 +2,13 @@ const isFactor = function (number, potentialFactor) {
   return number % potentialFactor == 0;
 };
 
-const factors = function (number, isFactor) {
+const factors = function (number) {
   const rootPod = Math.sqrt(number);
   const pod = Array(Math.floor(rootPod))
     .fill()
     .reduce((acc, curr, idx) => {
       if (isFactor(number, idx + 1)) {
-        if (number / (idx + 1) !== number) {
-          acc.add(number / (idx + 1));
-        }
+        acc.add(number / (idx + 1));
         acc.add(idx + 1);
       }
       return acc;
@@ -19,27 +17,20 @@ const factors = function (number, isFactor) {
   return pod;
 };
 
-const isPerfect = function (sum, factors, number, isFactor) {
-  return sum(factors(number, isFactor)) == number;
+const isPerfect = function (number) {
+  return sum(factors(number)) - number == number;
 };
 
-const isAbundant = function (sum, factors, number, isFactor) {
-  return sum(factors(number, isFactor)) > number;
+const isAbundant = function (number) {
+  return sum(factors(number)) - number > number;
 };
 
-const isDeficient = function (sum, factors, number, isFactor) {
-  return sum(factors(number, isFactor)) < number;
+const isDeficient = function (number) {
+  return sum(factors(number)) - number < number;
 };
 
 const sum = function (factors) {
   return [...factors].reduce((acc, curr) => acc + curr);
 };
-
-console.log(isFactor(10, 2));
-console.log(factors(10, isFactor));
-console.log(sum(factors(10, isFactor)));
-console.log(isAbundant(sum, factors, 10, isFactor));
-console.log(isPerfect(sum, factors, 10, isFactor));
-console.log(isDeficient(sum, factors, 10, isFactor));
 
 module.exports = { isFactor, factors, sum, isPerfect, isDeficient, isAbundant };
--- CS06/primeAlpha.js
+++ CS06/primeAlpha.js
@@ -1,38 +1,14 @@
-const isFactor = function (number, potentialFactor) {
-  return number % potentialFactor == 0;
-};
+const { isFactor, factors } = require("./classifierAlpha.js");
 
-const isPrime = function (number, equalSet, factors, isFactor) {
-  const primeSet = new Set([1, number]);
-  return this.number > 1 && equalSet(factors(number, isFactor), primeSet);
-};
-
-const factors = function (number, isFactor) {
-  const rootPod = Math.sqrt(number);
-  const pod = Array(Math.floor(rootPod))
-    .fill()
-    .reduce((acc, curr, idx) => {
-      if (isFactor(number, idx + 1)) {
-        if (number / (idx + 1) !== number) {
-          acc.add(number / (idx + 1));
-        }
-        acc.add(idx + 1);
-      }
-      return acc;
-    }, new Set());
-
-  return pod;
+const isPrime = function (number) {
+  return number > 1 && equalSet(factors(number), new Set([1, number]));
 };
 
 const equalSet = function (aset, bset) {
   const checkEqual = [...aset].filter((item) => {
     if ([...bset].includes(item)) return item;
   });
-  if (aset.size !== bset.size || checkEqual.length !== bset.size) {
-    return false;
-  }
-
-  return true;
+  return aset.size === bset.size && checkEqual.length === bset.size;
 };
 
 module.exports = { equalSet, isPrime };
