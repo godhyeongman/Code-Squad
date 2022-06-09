2801470a461e9992b9952caebb17bb8a6bbb650a
ldldz <qkrqudrjs4350@gmail.com>
2022-06-09 23:46:19 +0900

    feat: 현재 화면에 보이는 마커만 표시하기
    Co-authored-by: godhyeongman <godhyeongman@users.noreply.github.com>

--- airbnb/src/Components/Map/NaverMap.tsx
+++ airbnb/src/Components/Map/NaverMap.tsx
@@ -13,36 +13,76 @@ interface NaverMapProps {
   height: string;
 }
 
+function showMarker(map: any, marker: any) {
+  if (marker.setMap()) return;
+  marker.setMap(map);
+}
+
+function hideMarker(marker: any) {
+  if (!marker.setMap()) return;
+  marker.setMap(null);
+}
+
+function updateMarkers(map: any, markers: any[]) {
+  const mapBounds = map.getBounds();
+
+  markers.forEach(marker => {
+    const position = marker.getPosition();
+
+    if (mapBounds.hasLatLng(position)) {
+      showMarker(map, marker);
+    } else {
+      hideMarker(marker);
+    }
+  });
+}
+
+function makePriceMarker(
+  maps: any,
+  {
+    latitude,
+    longitude,
+    title,
+    price,
+  }: {
+    latitude: number;
+    longitude: number;
+    title: string;
+    price: number;
+  },
+) {
+  return new maps.Marker({
+    position: new maps.LatLng(latitude, longitude),
+    map: null,
+    title,
+    icon: {
+      content: renderToString(<PriceMarker price={price} />),
+      size: new maps.Size(1, 1),
+      anchor: new maps.Point(19, 58),
+    },
+  });
+}
+
 export function NaverMap({ width, height }: NaverMapProps) {
   useEffect(() => {
     const { naver } = window;
+    console.log(naver);
     const mapOptions = {
       center: new naver.maps.LatLng(37.491, 127.0339),
       zoom: 17,
     };
-    const map = new naver.maps.Map('map', mapOptions);
+    const baseMap = new naver.maps.Map('map', mapOptions);
+    const markers = coords.map(coord => makePriceMarker(naver.maps, coord));
 
-    function makePriceMarker(
-      targetMap: object,
-      {
-        latitude,
-        longitude,
-        title,
-        price,
-      }: { latitude: number; longitude: number; title: string; price: number },
-    ) {
-      const marker = new naver.maps.Marker({
-        position: new naver.maps.LatLng(latitude, longitude),
-        map: targetMap,
-        title,
-        icon: {
-          content: renderToString(<PriceMarker price={price} />),
-          size: new naver.maps.Size(1, 1),
-          anchor: new naver.maps.Point(19, 58),
-        },
-      });
-    }
-    coords.forEach(coord => makePriceMarker(map, coord));
+    naver.maps.Event.addListener(baseMap, 'zoom_changed', function () {
+      updateMarkers(baseMap, markers);
+    });
+
+    naver.maps.Event.addListener(baseMap, 'dragend', function () {
+      updateMarkers(baseMap, markers);
+    });
+
+    updateMarkers(baseMap, markers);
   }, []);
   return (
     <div>
